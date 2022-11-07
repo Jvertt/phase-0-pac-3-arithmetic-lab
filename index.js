5510ef7ff963e21ed440ad955a262756bcb633f1
Justin Verthein <Justinmackay88@hotmail.com>
2022-11-07 10:28:17 -1000

    commit tree 2c8f70a80de9eaad4a9f9c8a234b5a28c5e7308b

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a -b;
+}
+
+function multiply(a, b){
+    return a *b;
+}
+
+function divide(a, b){
+    return a/b;
+}
+
+function increment(n){
+    return n + 1;
+}
+
+function decrement(n){
+    return n - 1;
+}
+
+function makeInt(string){
+    parseInt("string", "10")
+    return string;
+}
+
+function preserveDecimal(string){
+    parseFloat("string", 10)
+    return string;
+}
